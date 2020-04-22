07e9da2600a6d71c0b10f1fe725433c4924a1d42
Leandro Neves <leandrosoares6@hotmail.com>
2020-04-21 22:31:48 -0300

    Integration with reactotron and redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
+import '~/config/ReactotronConfig';
+
 import { NavigationContainer } from '@react-navigation/native';
+import { store, persistor } from './store';
 
 import Routes from './routes';
 
@@ -11,8 +16,12 @@ import Routes from './routes';
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <Routes />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+          <Routes />
+        </PersistGate>
+      </Provider>
     </NavigationContainer>
   );
 }
